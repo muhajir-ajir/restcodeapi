1a653d67a1a669a26749c398b17714b67a661a98
muhajir-ajir <ghozanemozir@gmail.com>
2021-03-07 16:30:58 -0500

    membuat setting routes js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+//memanggil routes
+var routes = require('./routes');
+routes(app);
+
 app.listen(4000, () => {
     console.log(`Server started on port`);
 });
